e74468621614dd7f76ea5832288f52cda1822978
Dmytro <dimakryvak7@gmail.com>
2025-02-26 16:57:18 +0200

    v1.3(update) I write clearInterval

--- src/js/modules/timer.js
+++ src/js/modules/timer.js
@@ -1,16 +1,27 @@
 function timer() {
   const endDate = new Date();
   endDate.setDate(endDate.getDate() + 3);
-  // const endDate = new Date("March 23, 2025, 23:59:59");
+  //   const endDate = new Date("Feb 26, 2025, 16:54:59");
 
   const daysElement = document.querySelector("#days");
   const hoursElement = document.querySelector("#hours");
   const minutesElement = document.querySelector("#minutes");
   const secondsElement = document.querySelector("#seconds");
 
+  let timeInterval;
+
   function updateTimer() {
     const now = new Date();
     const timeDifference = endDate - now;
+
+    if (timeDifference <= 0) {
+      daysElement.innerText = "0";
+      hoursElement.innerText = "0";
+      minutesElement.innerText = "0";
+      secondsElement.innerText = "0";
+      clearInterval(timeInterval);
+      return;
+    }
 
     const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
     const hours = Math.floor(
@@ -29,7 +40,7 @@ function timer() {
 
   updateTimer();
 
-  setInterval(updateTimer, 1000);
+  timeInterval = setInterval(updateTimer, 1000);
 }
 
 timer();
